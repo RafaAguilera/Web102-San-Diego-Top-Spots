dcfb4b28e7a52a845cefb2924c0f95f27527627e
Rafael <aguilera.morales.rafa@gmail.com>
2022-03-23 16:47:43 -0700

    san diego top spots

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-// write your code here
+$(document).ready(function(){
+        var url="data.json";
+        $("#topSpots tbody").html("");
+        $.getJSON(url,function(SDTS){
+        $.each(SDTS, function(i,SDTS){
+        var newRow =
+        "<tr>"
+        +"<td>"+SDTS.name+"</td>"
+        +"<td>"+SDTS.description+"</td>"
+        +"<td>"+`<a href=https://www.google.com/maps?q=${SDTS.location}>link</a>`
+        +"</td>"
+        +"</tr>";
+        $(newRow).appendTo("#topSpots tbody");
+        });
+        });
+        
+        });
 
-});
+
+    
